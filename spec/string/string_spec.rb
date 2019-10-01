2eaa7dccf31698bacc4188a5cd38ce94cd5a1878
Jelani Woods <jelani@firstdraft.com>
2019-10-01 15:48:45 -0500

    The rest

--- spec/string/string_spec.rb
+++ spec/string/string_spec.rb
@@ -12,8 +12,20 @@ describe "strip.rb" do
   end
 end
 
-describe "strip.rb" do
+describe "case.rb" do
   it "should output 'remove outside spaces'" do
     expect { require_relative '../../string/case' }.to output("HELLO friends AnD FaMiLy\n").to_stdout
+  end
+end
+
+describe "gsub.rb" do
+  it "should output 'remove outside spaces'" do
+    expect { require_relative '../../string/gsub' }.to output("put spaces in between these words\n").to_stdout
+  end
+end
+
+describe "chomp.rb" do
+  it "should output 'remove outside spaces'" do
+    expect { require_relative '../../string/chomp' }.to output("Hello!\n").to_stdout
   end
 end
---
+++ string/gsub.rb
@@ -0,0 +1,2 @@
+sentence = "put_spaces_in_between_these_words"
+puts sentence.gsub("_", " ")
