f4b54f8e8df23644d36e067b523077e6aa4c85ba
Jelani Woods <jelani@firstdraft.com>
2019-10-02 11:10:12 -0500

    Write gets test

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-
-String multiplication
-upcase, downcase, swapcase
-chomp
-gsub
-strip
-
+# Ruby Practice
+## String
+- String multiplication
+- upcase, downcase, swapcase
+- chomp
+- gsub
+- strip
+- gets
+  - Hello, gets.chomp
+## Integer
+- short sides, => hypotenuse
+- basic math
+## Float
+- basic math, hypotenuse
+- rounding
+## Date
+## Array
+- square one element
+- count
+- reverse
+- lowest_number highest_number difference
+- sum
+## Conditionals
+- rps
+## Loops
+## Hash
+## Class
+- Person
+## Advanced (Exercism)
+- HelloWorld.hello
+- TwoFer.two_fer
+- Acronym.abbreviate
+- Hamming.compute
+- Raindrops.convert
+- Isogram.isogram?
--- spec/string/string_spec.rb
+++ spec/string/string_spec.rb
@@ -13,19 +13,35 @@ describe "strip.rb" do
 end
 
 describe "case.rb" do
-  it "should output 'remove outside spaces'" do
+  it "should output 'HELLO friends AnD FaMiLy'" do
     expect { require_relative '../../string/case' }.to output("HELLO friends AnD FaMiLy\n").to_stdout
   end
 end
 
 describe "gsub.rb" do
-  it "should output 'remove outside spaces'" do
+  it "should output a String of words separated by spaces" do
     expect { require_relative '../../string/gsub' }.to output("put spaces in between these words\n").to_stdout
   end
 end
 
 describe "chomp.rb" do
-  it "should output 'remove outside spaces'" do
+  it "should remove ending '$'" do
     expect { require_relative '../../string/chomp' }.to output("Hello!\n").to_stdout
+  end
+end
+
+describe "gets.rb" do
+  before do
+      $stdin = StringIO.new("jelani")
+  end
+
+  after do 
+      $stdin = STDIN
+  end
+  
+  it "should output 'Hello, name!'" do
+    allow($stdin).to receive(:gets).and_return("jelani")
+    name = $stdin.gets
+    expect { require_relative '../../string/gets' }.to output("Hello, Jelani!\n").to_stdout
   end
 end
