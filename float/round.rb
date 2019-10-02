66089052b0c5801d8660710fca833f812da46bef
Jelani Woods <jelani@firstdraft.com>
2019-10-02 11:32:54 -0500

    Float: round

---
+++ float/round.rb
@@ -0,0 +1,2 @@
+# Output 10 / 3 rounded to 3 decimal places.
+puts (10.to_f / 3).round(3)
--- spec/scripts/float_spec.rb
+++ spec/scripts/float_spec.rb
@@ -1,17 +1,14 @@
 
 describe "find_hypotenuse.rb" do
-  before do
-    $stdin = StringIO.new("2.8 4.5")
-  end
-
-  after do 
-      $stdin = STDIN
-  end
-
   it "should output hypotenuse" do
-    allow($stdin).to receive(:gets).and_return("2.8 4.5")
-    short_sides = $stdin.gets.chomp
     expect { require_relative '../../float/find_hypotenuse' }.to output("5.3\n").to_stdout
+
+  end
+end
+
+describe "round.rb" do
+  it "should output hypotenuse" do
+    expect { require_relative '../../float/round' }.to output("3.333\n").to_stdout
 
   end
 end
